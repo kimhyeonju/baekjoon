f1126c65bad15b8a56bc00199d6f892bdebf2ce6
Hyeon Ju <89682253+kimhyeonju@users.noreply.github.com>
2022-03-18 18:31:23 +0900

    baekjoon

--- baekjoon/bj_10814.java
+++ baekjoon/bj_10814.java
@@ -19,8 +19,10 @@ public class bj_10814 {
         Arrays.sort(str, new Comparator<String[]>() {
             @Override
             public int compare(String[] o1, String[] o2) {
-                return Integer.parseInt(o1[0])-Integer.parseInt(o2[0]);
+                return Integer.parseInt(o1[0])-Integer.parseInt(o2[0]);//오름차순
+                //return Integer.parseInt(o2[0]) - Integer.parseInt(o1[0]);//내림차순
             } // 같으면 0 , o1이 앞에 오게하려면 음수, o1이 뒤에 오게하려면 양수
+            //o1이 기준
         });
         for(int i = 0; i < n; i++){
             System.out.println(str[i][0] + " " + str[i][1]);
