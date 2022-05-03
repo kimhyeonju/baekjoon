77f535101a550f5d32c8493e8d8abd85ffea83c1
kimhyeonju <hyeonju0014@naver.com>
2022-05-03 16:40:44 +0900

    baekjoon

--- bj_15702.java
+++ bj_15702.java
@@ -14,23 +14,35 @@ public class bj_15702 {
         int[] arr2 = new int[arr[0]];
 
         for(int i =0; i < arr[0]; i++ ){
-            arr2[i] = Integer.parseInt(br.readLine().split("")[i]);
+            arr2[i] = Integer.parseInt(br.readLine().split(" ")[i]);
         }// 점수 크기
-
+        //System.out.println("1");
         String[][] point = new String[arr[1]][arr[0]+1];
         for(int i =0; i < arr[1]; i++ ){
             for(int j =0; j < arr[0]+1; j++){
                 point[i][j] = br.readLine().split(" ")[j];
             }
         }
+        //입력 다 담음
         int[] sum = new int[arr[1]];
+
         for(int i = 0; i < arr[1]; i++){
             for(int j =1; j < arr[0]+1; j++){
                 if(point[i][j] == "O"){
                     sum[i] += arr2[j-1];
+                }else {
+                    sum[i] += 0;
                 }
             }
-
         }
+        int max = sum[0];
+        int index = 0;
+        for(int i = 0; i < arr[1]; i++){
+            if(max<sum[i]){
+                max = sum[i];
+                index = i;
+            }
+        }
+        System.out.println(point[index][0] + max);
     }
 }
